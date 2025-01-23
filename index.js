93c774c76abb338531907db5d500d3b9da55d267
somya167 <somya0519@gmail.com>
2025-01-22 22:01:35 -0500

    added port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the public folder
 app.use(express.static('public'));
